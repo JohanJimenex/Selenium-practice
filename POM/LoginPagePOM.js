8d83e266be644b0ac7234d82be7422e13274c388
johanjimenex <johan.jimenez.23@gmail.com>
2024-06-13 23:56:39 -0400

    first commit

---
+++ POM/LoginPagePOM.js
@@ -0,0 +1,21 @@
+const { By } = require("selenium-webdriver");
+
+class LoginPagePOM {
+  driver;
+
+  usernameField = By.id("user-name");
+  passwordField = By.id("password");
+  loginButton = By.id("login-button");
+
+  constructor(driver) {
+    this.driver = driver;
+  }
+
+  async login(username, password) {
+    await this.driver.findElement(this.usernameField).sendKeys(username);
+    await this.driver.findElement(this.passwordField).sendKeys(password);
+    await this.driver.findElement(this.loginButton).click();
+  }
+}
+
+module.exports = LoginPagePOM;
---
+++ Test/Login Page - Test Case.spec.js
@@ -0,0 +1,25 @@
+const { By, Builder, Browser } = require("selenium-webdriver");
+const assert = require("assert");
+
+describe("Login Page Test Case", function () {
+  let driver;
+
+  before(async () => {
+    driver = await new Builder().forBrowser(Browser.CHROME).build();
+    await driver.get("https://www.saucedemo.com/v1/index.html");
+  });
+
+  it("Login valid credentials", async () => {
+    let username = await driver.findElement(By.id("user-name"));
+    let password = await driver.findElement(By.id("password"));
+    let loginButton = await driver.findElement(By.id("login-button"));
+
+    await username.sendKeys("standard_user");
+    await password.sendKeys("secret_sauce");
+    await loginButton.click();
+
+    let products = await driver.findElement(By.className("product_label"));
+    let text = await products.getText();
+    assert.equal(text, "Products");
+  });
+});
